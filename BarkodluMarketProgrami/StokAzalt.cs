7ec685195dac96fb630d20d83dd72a2ce85bc751
Eyüp Çelik <eyupturkey641@gmail.com>
2025-06-22 03:10:05 +0300

    Stok azaltma işlemi yapıldı

---
+++ BarkodluMarketProgrami/StokAzalt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkodluMarketProgrami
+{
+    internal class StokAzalt
+    {
+        public static void stokAzalt(string barkod, double miktar)
+        {
+            using (var db = new BarkodEntities())
+            {
+                var urun = db.Urun.SingleOrDefault(x => x.Barkod == barkod);
+                if (urun != null)
+                {
+                    urun.Miktar = urun.Miktar - miktar;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Ürün bulunamadı.");
+                }
+            }
+        }
+    }
+}
