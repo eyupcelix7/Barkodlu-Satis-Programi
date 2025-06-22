f4ce68be970ab2064cce15238d939e354563644f
Eyüp Çelik <eyupturkey641@gmail.com>
2025-06-22 03:09:51 +0300

    Stok arttırma işlemi yapıldı

---
+++ BarkodluMarketProgrami/StokArttir.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkodluMarketProgrami
+{
+    internal class StokArttir
+    {
+        public static void stokArttir(string barkod, double miktar)
+        {
+            using (var db = new BarkodEntities())
+            {
+                var urun = db.Urun.SingleOrDefault(x => x.Barkod == barkod);
+                if (urun != null)
+                {
+                    urun.Miktar = urun.Miktar + miktar;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Ürün bulunamadı.");
+                }
+            }
+        }
+    }
+}
