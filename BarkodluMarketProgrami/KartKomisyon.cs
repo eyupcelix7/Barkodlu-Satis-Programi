065614fd37b24c65068c5c687ac7b5243ac145e6
Eyüp Çelik <eyupturkey641@gmail.com>
2025-06-29 01:38:43 +0300

    Komisyon sınıfı oluşturuldu

---
+++ BarkodluMarketProgrami/KartKomisyon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkodluMarketProgrami
+{
+    internal class KartKomisyon
+    {
+        public double KartKomisyonHesapla() {
+            using (var db = new BarkodEntities())
+            {
+                if (db.Ayarlar.Any())
+                {
+                    return (double) db.Ayarlar.First().KartKomisyon;
+                }
+                else
+                {
+                    return (double) 0;
+                }
+            }
+
+        }
+    }
+}
