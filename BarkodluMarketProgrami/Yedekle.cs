51407cea19685dd5ed8a6c3f834bd07faab17657
Eyüp Çelik <eyupturkey641@gmail.com>
2025-07-02 05:03:47 +0300

    Yedekleme işlemi yapıldı

---
+++ BarkodluMarketProgrami/Yedekle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarkodluMarketProgrami
+{
+    internal class Yedekle
+    {
+        public Yedekle()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Veri Yedek Dosyası|0.bak";
+            saveFileDialog.FileName = "Barkodlu Satış Programı-" + DateTime.Now.ToShortDateString();
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+
+                    var dbHedef = saveFileDialog.FileName;
+                    string dbKaynak = @"C:\dbs\Barkod.mdf";
+                    using(var db = new BarkodEntities())
+                    {
+                        var cmd = @"BACKUP DATABASE[" + dbKaynak + "] TO DISK='" + dbHedef + "'";
+                        db.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction,cmd);
+                    }
+                    Cursor.Current = Cursors.Default;
+                    MessageBox.Show("Yedekleme Tamamlandı","Başarılı",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                }
+                catch(Exception e)
+                {
+                        MessageBox.Show(e.ToString());
+                }
+            }
+        }
+    }
+}
