c13d8ab8a24aecdf7ff1f89aa32c0b41a35fb15e
Eyüp Çelik <eyupturkey641@gmail.com>
2025-07-05 04:12:50 +0300

    Veresiye için yeni kullanıcı ekleme yapıldı

---
+++ BarkodluMarketProgrami/FormVeresiyeYeniKullanici.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace BarkodluMarketProgrami
+{
+    partial class FormVeresiyeYeniKullanici
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKasiyer = new System.Windows.Forms.Label();
+            this.lblKullanici = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtTelNo = new System.Windows.Forms.TextBox();
+            this.lblTelNo = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.rtbAciklama = new System.Windows.Forms.RichTextBox();
+            this.lblAciklama = new System.Windows.Forms.Label();
+            this.cbxKayitliListesi = new System.Windows.Forms.ComboBox();
+            this.lblKayitliListesi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblKasiyer
+            // 
+            this.lblKasiyer.AutoSize = true;
+            this.lblKasiyer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKasiyer.ForeColor = System.Drawing.Color.Black;
+            this.lblKasiyer.Location = new System.Drawing.Point(2, 9);
+            this.lblKasiyer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblKasiyer.Name = "lblKasiyer";
+            this.lblKasiyer.Size = new System.Drawing.Size(69, 18);
+            this.lblKasiyer.TabIndex = 107;
+            this.lblKasiyer.Text = "Kasiyer:";
+            // 
+            // lblKullanici
+            // 
+            this.lblKullanici.AutoSize = true;
+            this.lblKullanici.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullanici.ForeColor = System.Drawing.Color.Black;
+            this.lblKullanici.Location = new System.Drawing.Point(69, 11);
+            this.lblKullanici.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblKullanici.Name = "lblKullanici";
+            this.lblKullanici.Size = new System.Drawing.Size(71, 16);
+            this.lblKullanici.TabIndex = 106;
+            this.lblKullanici.Text = "eyupcelix7";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label7.Location = new System.Drawing.Point(108, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 20);
+            this.label7.TabIndex = 103;
+            this.label7.Text = "*";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.AutoSize = true;
+            this.btnKaydet.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnKaydet.FlatAppearance.BorderColor = System.Drawing.Color.OrangeRed;
+            this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.ForeColor = System.Drawing.Color.White;
+            this.btnKaydet.Location = new System.Drawing.Point(16, 405);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(1);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(119, 46);
+            this.btnKaydet.TabIndex = 91;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtTelNo
+            // 
+            this.txtTelNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTelNo.Location = new System.Drawing.Point(16, 163);
+            this.txtTelNo.MaxLength = 11;
+            this.txtTelNo.Name = "txtTelNo";
+            this.txtTelNo.Size = new System.Drawing.Size(216, 26);
+            this.txtTelNo.TabIndex = 88;
+            // 
+            // lblTelNo
+            // 
+            this.lblTelNo.AutoSize = true;
+            this.lblTelNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTelNo.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblTelNo.Location = new System.Drawing.Point(12, 124);
+            this.lblTelNo.Name = "lblTelNo";
+            this.lblTelNo.Size = new System.Drawing.Size(175, 24);
+            this.lblTelNo.TabIndex = 87;
+            this.lblTelNo.Text = "Telefon Numarası";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(16, 85);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(216, 26);
+            this.txtAdSoyad.TabIndex = 86;
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAdSoyad.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblAdSoyad.Location = new System.Drawing.Point(12, 46);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(100, 24);
+            this.lblAdSoyad.TabIndex = 85;
+            this.lblAdSoyad.Text = "Ad Soyad";
+            // 
+            // rtbAciklama
+            // 
+            this.rtbAciklama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbAciklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rtbAciklama.Location = new System.Drawing.Point(16, 247);
+            this.rtbAciklama.Name = "rtbAciklama";
+            this.rtbAciklama.Size = new System.Drawing.Size(216, 125);
+            this.rtbAciklama.TabIndex = 108;
+            this.rtbAciklama.Text = "";
+            // 
+            // lblAciklama
+            // 
+            this.lblAciklama.AutoSize = true;
+            this.lblAciklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAciklama.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblAciklama.Location = new System.Drawing.Point(12, 205);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Size = new System.Drawing.Size(94, 24);
+            this.lblAciklama.TabIndex = 109;
+            this.lblAciklama.Text = "Açıklama";
+            // 
+            // cbxKayitliListesi
+            // 
+            this.cbxKayitliListesi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxKayitliListesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cbxKayitliListesi.FormattingEnabled = true;
+            this.cbxKayitliListesi.Location = new System.Drawing.Point(332, 83);
+            this.cbxKayitliListesi.Name = "cbxKayitliListesi";
+            this.cbxKayitliListesi.Size = new System.Drawing.Size(189, 28);
+            this.cbxKayitliListesi.TabIndex = 110;
+            // 
+            // lblKayitliListesi
+            // 
+            this.lblKayitliListesi.AutoSize = true;
+            this.lblKayitliListesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKayitliListesi.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblKayitliListesi.Location = new System.Drawing.Point(328, 49);
+            this.lblKayitliListesi.Name = "lblKayitliListesi";
+            this.lblKayitliListesi.Size = new System.Drawing.Size(128, 24);
+            this.lblKayitliListesi.TabIndex = 111;
+            this.lblKayitliListesi.Text = "Kayıtlı Listesi";
+            // 
+            // FormVeresiyeYeniKullanici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 515);
+            this.Controls.Add(this.lblKayitliListesi);
+            this.Controls.Add(this.cbxKayitliListesi);
+            this.Controls.Add(this.lblAciklama);
+            this.Controls.Add(this.rtbAciklama);
+            this.Controls.Add(this.lblKasiyer);
+            this.Controls.Add(this.lblKullanici);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtTelNo);
+            this.Controls.Add(this.lblTelNo);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Name = "FormVeresiyeYeniKullanici";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yeni Kullanıcı Oluştur";
+            this.Load += new System.EventHandler(this.FormVeresiyeYeniKullanici_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKasiyer;
+        public System.Windows.Forms.Label lblKullanici;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtTelNo;
+        private System.Windows.Forms.Label lblTelNo;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.RichTextBox rtbAciklama;
+        private System.Windows.Forms.Label lblAciklama;
+        private System.Windows.Forms.ComboBox cbxKayitliListesi;
+        private System.Windows.Forms.Label lblKayitliListesi;
+    }
+}
---
+++ BarkodluMarketProgrami/FormVeresiyeYeniKullanici.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarkodluMarketProgrami
+{
+    public partial class FormVeresiyeYeniKullanici : Form
+    {
+        public FormVeresiyeYeniKullanici()
+        {
+            InitializeComponent();
+        }
+        private void FormVeresiyeYeniKullanici_Load(object sender, EventArgs e)
+        {
+            kayitliKullanicilariGetir();
+        }
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            if(txtAdSoyad.Text != "")
+            {
+                string adSoyad = txtAdSoyad.Text.Trim();
+                string telNo = txtTelNo.Text.Trim();
+                string aciklama = rtbAciklama.Text.Trim();
+                using(var db = new BarkodEntities())
+                {
+                    if(telNo != "")
+                    {
+                        if(db.VeresiyeKullanicilar.Where(x => x.Telefon != telNo).Any())
+                        {
+                            VeresiyeKullanicilar vK = new VeresiyeKullanicilar();
+                            vK.AdSoyad = adSoyad;
+                            vK.Telefon = telNo;
+                            vK.Aciklama = aciklama;
+                            db.VeresiyeKullanicilar.Add(vK);
+                            db.SaveChanges();
+                            MessageBox.Show("Kayıt Başarıyla Oluşturuldu\nAd Soyad: "+adSoyad+"\nTelefon Numarası: "+telNo+"\nAçıklama: "+aciklama, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Temizle();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Bu Telefon Numarası Ile Daha Önce Kayıt Yapılmış", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        VeresiyeKullanicilar vK = new VeresiyeKullanicilar();
+                        vK.AdSoyad = adSoyad;
+                        vK.Telefon = telNo;
+                        vK.Aciklama = aciklama;
+                        db.VeresiyeKullanicilar.Add(vK);
+                        db.SaveChanges();
+                        MessageBox.Show("Kayıt Başarıyla Oluşturuldu\n\nAd Soyad: " + adSoyad + "\nTelefon Numarası: " + telNo + "\nAçıklama: " + aciklama, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Temizle();
+                    }
+
+                }
+            }
+        }
+        private void Temizle()
+        {
+            txtAdSoyad.Text = string.Empty;
+            txtTelNo.Text = string.Empty;
+            rtbAciklama.Text = string.Empty;
+            kayitliKullanicilariGetir();
+        }
+        private void kayitliKullanicilariGetir()
+        {
+            using (var db = new BarkodEntities())
+            {
+                var kayitliKullanicilar = db.VeresiyeKullanicilar.Select(x => x.AdSoyad).ToList();
+                cbxKayitliListesi.DataSource = kayitliKullanicilar;
+            }
+        }
+    }
+}
